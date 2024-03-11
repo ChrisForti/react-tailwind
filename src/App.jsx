3bfe77a2420f0d350413c943b79bde98089438c2
Christopher Forti <christopher.forti.79@gmail.com>
2024-03-10 21:41:42 -0400

    Reverted what i did in the last commit to app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import useState from "react";
-import CountButton from "./assets/components/CountButton";
+import { useState } from "react";
+import { CountButton } from "./assets/components/CountButton";
 import Nav from "./assets/components/Nav";
 import "./App.css";
 import Header from "./assets/components/Header";
@@ -12,7 +12,9 @@ function App() {
   return (
     <>
       <div>
-        <CountButton />
+        <div>
+          <CountButton />
+        </div>
         <Nav />
       </div>
 
