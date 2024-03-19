fffd27de06e8ddfc5a86ea82b6c0416d2f1dfcc1
Christopher Forti <christopher.forti.79@gmail.com>
2024-03-19 17:45:08 -0400

    Put tic tac toe game in, and removed the nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import { CountButton } from "./assets/components/CountButton";
-import Nav from "./assets/components/Nav";
+
 import "./App.css";
 import Header from "./assets/components/Header";
 import Footer from "./assets/components/Footer";
 import NameFunc from "./assets/components/NameFunction";
+import TicTacToe from "./assets/components/TicTacToe";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,12 +19,14 @@ function App() {
         </div>
         <h2>Reactor</h2>
       </div>
-      <Nav />
+
       <div>
         <NameFunc name="Christopher" />
         <CountButton />
       </div>
-      <div></div>
+      <div>
+        <TicTacToe />
+      </div>
       <div></div>
       <div>
         <Footer />
